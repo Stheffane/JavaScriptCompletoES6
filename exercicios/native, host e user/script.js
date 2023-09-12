bed6fc4c2e3ec8aa76287e86b44ebdf83b5bf4c8
stheffane.nunes <snunes@iouu.com.br>
2023-09-11 23:00:27 -0300

    feat: native, host e user

---
+++ exercicios/native, host e user/script.js
@@ -0,0 +1,20 @@
+// Liste 5 objetos nativos
+Object
+String
+Array
+Function
+Number
+Date
+Math
+// ...
+
+// Liste 5 objetos do browser
+Window
+Document
+HTMLAllCollection
+NodeList
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+document.webkitVisibilityState
+document.webkitHidden
